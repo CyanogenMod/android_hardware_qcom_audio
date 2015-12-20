b4f6e4e3e17a37e35552bba6b7b1e68dabcced6d
AdrianDC <radian.dc@gmail.com>
2015-12-21 00:04:36 +0100

    audio-caf-8960: Fix log as error on successful mixer
    Change-Id: I0b4efb67d28e4c8ec02979f38a65d1dc879ed22d
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- legacy/libalsa-intf/alsa_ucm.c
+++ legacy/libalsa-intf/alsa_ucm.c
@@ -3530,10 +3530,9 @@ char **nxt_str, int verb_index, int ctrl_list_type)
         } else if (strcasestr(current_str, "EC_REF_RXMixerCTL") != NULL) {
             ret = snd_ucm_extract_ec_ref_rx_mixer_ctl(current_str,
                       &list->ec_ref_rx_mixer_ctl);
-            ALOGV("EC_REF_RX mixer ctl: ret:%d\n", ret);
             if (ret < 0)
                 break;
-            ALOGE("EC_REF_RX mixer ctl: %s\n", list->ec_ref_rx_mixer_ctl);
+            ALOGV("EC_REF_RX mixer ctl: %s\n", list->ec_ref_rx_mixer_ctl);
         }
         if (strcasestr(current_str, "EnableSequence") != NULL) {
             controls_count = get_controls_count(next_str);
